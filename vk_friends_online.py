8648c1ff226c27d7b257d69e286d96164a793523
noerten <darkstria@gmail.com>
2016-12-28 16:39:10 +0300

    initial

--- vk_friends_online.py
+++ vk_friends_online.py
@@ -1,15 +1,30 @@
+try:
+    import config
+except:
+    pass
+import getpass
+
 import vk
 
 
-APP_ID = -1  # чтобы получить app_id, нужно зарегистрировать своё приложение на https://vk.com/dev
+APP_ID = '5798454'
 
 
 def get_user_login():
-    pass
+    user_login = input("Enter your login or press 'Enter' to get it from"
+                       " config.py, that is placed in the script's folder: ")
+    if not user_login:
+        user_login = config.USER_LOGIN
+    return user_login
 
 
 def get_user_password():
-    pass
+    user_password = getpass.getpass("Enter your password or press 'Enter' to "
+                                    "get it from config.py, that is placed in "
+                                    "the folder with script: ")
+    if not user_password:
+        user_password = config.USER_PASSWORD
+    return user_password
 
 
 def get_online_friends(login, password):
@@ -17,13 +32,19 @@ def get_online_friends(login, password):
         app_id=APP_ID,
         user_login=login,
         user_password=password,
-    )
+        scope='friends',
+        )
     api = vk.API(session)
-    # например, api.friends.get()
+    friends_online_ids = api.friends.getOnline()
+    friends_online = api.users.get(user_ids=friends_online_ids)
+    return friends_online
 
 
 def output_friends_to_console(friends_online):
-    pass
+    print('Friends online:')
+    for friend in friends_online:
+        print(friend['first_name'], friend['last_name'])
+
 
 if __name__ == '__main__':
     login = get_user_login()
